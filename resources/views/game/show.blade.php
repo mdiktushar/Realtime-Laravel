9977ef65a5fbf0b1b2174ae8f5f522af9f281524
mdiktushar <md.tushar1804@gmail.com>
2022-05-28 15:14:56 +0600

    realtime game complete

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -41,7 +41,7 @@
                         <p id="winner" class="dispaly-1 d-none text-primary"></p>
                         <div class="text-center">
                             <label class="font-wigth-bold h5"></label>
-                            <select name="" id="" class="custom-select col-auto">
+                            <select name="" id="bet" class="custom-select col-auto">
                                 <option selected value="">Not in</option>
                                 @foreach (range(1, 12) as $number)
                                     <option value="">{{$number}}</option>                                    
@@ -49,7 +49,7 @@
                             </select>
                             <hr>
                             <p class="font-weight-bold h5">Ramaining Time</p>
-                            <p id="time" class="h5 text-danger">Waiting to start</p>
+                            <p id="timer" class="h5 text-danger">Waiting to start</p>
                             <hr>
                             <p id="result" class="h1"></p>
                         </div>
@@ -63,6 +63,41 @@
 
 @push('scripts')
     <script>
+        const circleElement = document.getElementById('circle');
+        const timerElement = document.getElementById('timer');
+        const winnerElement = document.getElementById('winner');
+        const betElement = document.getElementById('bet');
+        const resultElement = document.getElementById('result');
 
+        Echo.channel('game')
+        .listen('RemainingTimeChanged', (e) => {
+            timerElement.innerText = e.time;
+
+            circleElement.classList.add('refresh');
+
+            winnerElement.classList.add('d-none');
+
+            resultElement.innerText = '';
+            resultElement.classList.remove('text-success');
+            resultElement.classList.remove('text-danger');
+
+        })
+        .listen('WinnerNumberGenerated', (e) => {
+            circleElement.classList.remove('refresh');
+
+            let winner = e.number;
+            winnerElement.innerText = winner;
+            winnerElement.classList.remove('d-none');
+
+            let bet = betElement[betElement.selectedIndex].innerText;
+
+            if (bet == winner) {
+                resultElement.innerText = "You WIN";
+                resultElement.classList.add('text-success');
+            } else {
+                resultElement.innerText = "You LOSE";
+                resultElement.classList.add('text-danger');
+            }
+        })
     </script>
 @endpush
